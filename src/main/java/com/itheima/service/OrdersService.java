2f978b4b336261316161ba366a0c84e6d61cdfd8
LMWC <lmw2357520@qq.com>
2022-07-02 10:55:45 +0800

    项目实战——完成剩余功能

--- src/main/java/com/itheima/service/OrdersService.java
+++ src/main/java/com/itheima/service/OrdersService.java
@@ -7,4 +7,10 @@ public interface OrdersService extends IService<Orders> {
 
     //用户下单：保存订单信息到订单表和订单详情表中
     boolean submit(Orders orders);
+
+    /**
+     * 再来一单
+     * @param orders
+     */
+    void again(Orders orders);
 }
